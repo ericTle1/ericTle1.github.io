bc0cc846425c6150f4a7e565340f57f25163d363
ericTle1 <57995708+ericTle1@users.noreply.github.com>
2020-10-08 16:56:38 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,45 +1,49 @@
 let myFullName = "Eric Le"; // 16 characters in name...not including space
 
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
-document.querySelector(".header").innerHTML = myFullName + "\'s Lab 4\n";
+const name = document.querySelector(".header");
+name.innerText = myFullName + "'s Lab 4.";
 
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
-var newParagraphOne = document.createElement("p1");
-var newParagraphTwo = document.createElement("p2");
-var newParagraphOneText = document.createTextNode("First Paragraph: \n");
-var newParagraphTwoText = document.createTextNode("Second Paragraph:\n");
+const paragraph = document.querySelector(".content");
 
-newParagraphOne.appendChild(newParagraphOneText);
-newParagraphTwo.appendChild(newParagraphTwoText);
+const mp1 = document.createElement("p");
+mp1.className = "content1";
+mp1.innerText = "Hello wolrd!";
+paragraph.appendChild(mp1);
 
-var newContentParagraphs = document.querySelector(".content");
-newContentParagraphs.appendChild(newParagraphOne);
-newContentParagraphs.appendChild(newParagraphTwo);
+const mp2 = document.createElement("p");
+mp2.className = "content2"
+mp2.innerText = "Hello back at you!";
+paragraph.appendChild(mp2);
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-var newPhrase = "My name has " + myFullName.length + " characters.";
-newParagraphOne.append(newPhrase);
+const getmp1 = document.querySelector(".content1")
+let length = myFullName.length - 1
+mp1.innerText = "My name has " + length + " characters.";
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
-var thirdCharacterInFullName = myFullName.charAt(2);
+const getmp2 = document.querySelector(".content2")
+let thirdChar = myFullName[2].toUpperCase();
 
-newParagraphTwo.append(thirdCharacterInFullName);
+mp2.innerText = "The 3rd letter of name is " + thirdChar
 
 // 6 Add a new line to your second paragraph
-newParagraphTwo.append("\n");
+mp2.innerText = "The 3rd letter of my name is " + thirdChar + ". \n"
 
 // 7 Return the final three characters of your last name to that new line
-var finalThreeCharactersInFullName = myFullName.substring(myFullName.length - 3, myFullName.length);
-newParagraphTwo.append(finalThreeCharactersInFullName);
+const lastChars = myFullName.substring(myFullName.length - 3, myFullName.length);
+mp2.innerText = thirdChar + ": is the third letter of my first name" +". \n" + "The last 3 letters of my  last name are: " + lastChars.toUpperCase(lastChars);
+
 
 // 8 Substring your first and last name into two separate variables
-var firstName = myFullName.substring(0, 3);
-var lastName = myFullName.substring(13, 18);
+const firstN = myFullName.substring(0,4);
+const lastN = myFullName.substring(5,7);
 
 // 9 Add the total length of your first and last names together
-var nameLength = firstName.length + lastName.length;
+const total = firstN.length + lastN.length;
 
 // 10 Display that total next to your name in your header
-document.querySelector(".header").append("The length of my first and last name is: " + nameLength);
+document.querySelector(".header").append(" There are " + total + " letters in my Full Name.");
