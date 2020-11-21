e747b6df7e9eb08d50845de3b0ee17a9309d9d73
ericTle1 <57995708+ericTle1@users.noreply.github.com>
2020-11-20 21:47:01 -0500

    Update lab8script.js

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -1,4 +1,3 @@
-
 let url = "https://www.reddit.com/r/popular.json";
 let chart = "";
 console.log(url);
@@ -7,13 +6,20 @@ console.log(url);
 let settings = { method: "Get" };
 let chartValues = [];
 
+// Embedding script tag to enable CanvasJS
+let canvasJS = document.createElement('script');
+canvasJS.setAttribute('async', 'true')
+canvasJS.src = 'https://canvasjs.com/assets/script/canvasjs.min.js';
+document.querySelector('head').appendChild(canvasJS)
+
 async function getData() {
     await fetch(url, settings)
         .then(res => res.json())
         .then((json) => {
             let listSize = json.data.children.length;
+            let currentList = [];
             // Loop to pick 5 random entries
-            for (x = 0; x < 5; x++) {
+            for (let x = 0; x < 5; x++) {
                 /*
                     Get a random number within the size of the list
                     Get subreddit, author, title, and ups from record
@@ -22,17 +28,39 @@ async function getData() {
                     Add a new <li> element with the message to the 'redditList' element
                     Add a data entry to chartValues with author as the label and ups as the y component
                 */
-    
+                let random = Math.floor(Math.random() * Math.floor(listSize));
+                if (currentList.length < 2) {
+                    currentList.push(currentList);
+                } else {
+                    while (currentList.includes(random)) {
+                        random = Math.floor(Math.random() * Math.floor(listSize));
+                    }
+                    currentList.push(currentList);
+                }
+                let record = json.data.children[random]; // Random record
+                
+                let subreddit = record.data.subreddit; // Subreddit 
+                let author = record.data.author; // Author
+                let title = record.data.title; // Title
+                let ups = record.data.ups; // Upvotes
+                
+                let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
+                
+                let newLI = document.createElement("li");
+                newLI.innerHTML = message;
+                document.querySelector("#redditList").appendChild(newLI);
 
                 /*.......*/
                 
-                let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
+                let addToChart = {'label': author, y: ups}; // Gave this. This needs to be added to the 'chartValues'
                 /*.......*/
+
+                chartValues.push(addToChart);
             }
         })
         .then(values => console.log(chartValues));
-        //chart.render(); // Do you need to remove the comments from here in order to get it to work?
-};
+        chart.render(); // Do you need to remove the comments from here in order to get it to work? Yup!
+}
 
 window.onload = async function makeChart() {
     getData();
@@ -45,7 +73,7 @@ window.onload = async function makeChart() {
             { 
                 type: "column",
                 name: "Popular Reddit",
-                dataPoints: // WHAT GOES HERE???
+                dataPoints: chartValues // WHAT GOES HERE???
             }
         ]
     });
@@ -53,4 +81,4 @@ window.onload = async function makeChart() {
     chart.render();
 }
 
-//window.onload = makeChart();
+window.onload = makeChart();
